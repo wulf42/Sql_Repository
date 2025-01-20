11ceaafdfaed76a4f24adc9e6e96eae68e640ec1
Wojciech Pakoca <pakocawojciech@gmail.com>
2025-01-20 18:38:39 +0100

    Select for orders completed today

---
+++ Select/OrdersMadeToday.sql
@@ -0,0 +1,10 @@
+SELECT 
+	u.email,
+    u.address, 
+    p.name,
+    od.quantity
+FROM "ordersDetails" AS od
+JOIN "orders" AS o ON od.orderId = o.id 
+JOIN "products" AS p ON od.productId = p.id
+JOIN "users" AS u ON u.id = o.userId
+where o.createdat::date = CURRENT_DATE;
